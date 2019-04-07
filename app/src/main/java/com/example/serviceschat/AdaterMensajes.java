1a84bc9822df47e28a9b26e14379ada23dac3931
DANY <efelipes1@miumg.edu.gt>
2019-04-07 16:06:27 -0600

    creando cuerpo de mensaje

---
+++ app/src/main/java/com/example/serviceschat/AdaterMensajes.java
@@ -0,0 +1,23 @@
+package com.example.serviceschat;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class AdaterMensajes extends RecyclerView.Adapter<HolderMensaje> {
+    @NonNull
+    @Override
+    public HolderMensaje onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HolderMensaje holderMensaje, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/example/serviceschat/MainActivity.java
+++ app/src/main/java/com/example/serviceschat/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.serviceschat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CircleImageView fotoPerfil;
+    private TextView nombre;
+    private RecyclerView rvMensajes;
+    private EditText txtMensaje;
+    private Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fotoPerfil=(CircleImageView) findViewById(R.id.fotoPerfil);
+        nombre=(TextView) findViewById(R.id.nombre);
+        rvMensajes=(RecyclerView) findViewById(R.id.rvMensajes);
+        txtMensaje=(EditText) findViewById(R.id.txtMensaje);
+        btnEnviar=(Button) findViewById(R.id.btnEnviar);
+
     }
 }
