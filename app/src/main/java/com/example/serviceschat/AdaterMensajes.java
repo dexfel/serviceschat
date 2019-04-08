84d41adfa774229a8a4585e5535c8ee3b2a9e510
DANY <efelipes1@miumg.edu.gt>
2019-04-08 11:23:18 -0600

    variables mensajes

--- app/src/main/java/com/example/serviceschat/AdaterMensajes.java
+++ app/src/main/java/com/example/serviceschat/AdaterMensajes.java
@@ -1,10 +1,21 @@
 package com.example.serviceschat;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class AdaterMensajes extends RecyclerView.Adapter<HolderMensaje> {
+    private List<Mensaje> listMensaje;
+    private Context c;
+
+    public AdaterMensajes(List<Mensaje> listMensaje, Context c) {
+        this.listMensaje = listMensaje;
+        this.c = c;
+    }
+
     @NonNull
     @Override
     public HolderMensaje onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
