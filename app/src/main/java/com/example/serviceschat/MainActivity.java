a4d40c6ff664ad58a28116fb831192de2d1523ae
DANY <efelipes1@miumg.edu.gt>
2019-04-08 14:59:01 -0600

    conexion base de datos firebase exitosa

--- app/src/main/java/com/example/serviceschat/MainActivity.java
+++ app/src/main/java/com/example/serviceschat/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.serviceschat;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +12,12 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,8 +27,11 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView rvMensajes;
     private EditText txtMensaje;
     private Button btnEnviar;
-
     private AdapterMensajes adapter;
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
         txtMensaje=(EditText) findViewById(R.id.txtMensaje);
         btnEnviar=(Button) findViewById(R.id.btnEnviar);
 
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("chat");
+
         adapter = new AdapterMensajes(this);
         LinearLayoutManager l = new LinearLayoutManager(this);
         rvMensajes.setLayoutManager(l);
@@ -40,14 +55,43 @@ public class MainActivity extends AppCompatActivity {
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-         adapter.addMensaje(new Mensaje(txtMensaje.getText().toString(),nombre.getText().toString(),"","1","00:00"));
+         databaseReference.push().setValue(new Mensaje(txtMensaje.getText().toString(),nombre.getText().toString(),"","1","00:00"));
             }
         });
+
         adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
             @Override
             public void onItemRangeInserted(int positionStart, int itemCount) {
                 super.onItemRangeInserted(positionStart, itemCount);
                 setScrollbar();
+            }
+        });
+
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+             Mensaje m =dataSnapshot.getValue(Mensaje.class);
+                adapter.addMensaje(m);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
